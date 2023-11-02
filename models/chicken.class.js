a2fa9d2027b64f96bbc8781ca880fb8c2752b774
Alexander Riedel <alexander.riedel@gus-group.com>
2023-11-02 23:04:58 +0100

    refactoring

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -4,7 +4,7 @@ class Chicken extends MovableObject {
     x = 2000 + Math.random() * 4800; // Spawn
     height = 50;
     width = 40;
-    speed = 0.30 + Math.random() * (2 + Math.random()); // Speed
+    speed = 0.30 + Math.random() * (3 + Math.random()); // Speed
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
