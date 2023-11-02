1701dd3f905f51b5cf26a318503362584329742c
Alexander Riedel <alexander.riedel@gus-group.com>
2023-11-02 20:28:07 +0100

    add levels

---
+++ models/level.class.js
@@ -0,0 +1,11 @@
+class Level {
+    clouds;
+    backgroundObjects;
+    enemies;
+
+    constructor(enemies, clouds, backgroundObjects) {
+        this.enemies = enemies;
+        this.clouds = clouds;
+        this.backgroundObjects = backgroundObjects;
+    }
+}
