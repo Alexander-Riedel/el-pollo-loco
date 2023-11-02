adfab4b84b8e69f128ff35a9d02515491eccbc1d
Alexander Riedel <alexander.riedel@gus-group.com>
2023-11-02 20:13:07 +0100

    add background objects

--- models/world.class.js
+++ models/world.class.js
@@ -5,10 +5,26 @@ class World {
         new Cloud('img/5_background/layers/4_clouds/2.png')
     ];
     backgroundObjects = [
+        new BackgroundObject('img/5_background/layers/air.png', -719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
         new BackgroundObject('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0)
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
+        new BackgroundObject('img/5_background/layers/air.png', 719),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
+        new BackgroundObject('img/5_background/layers/air.png', 1438),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 1438),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 1438),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 1438),
+        new BackgroundObject('img/5_background/layers/air.png', 2157),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 2157),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 2157),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 2157),
     ];
     enemies = [
         new Chicken(),
