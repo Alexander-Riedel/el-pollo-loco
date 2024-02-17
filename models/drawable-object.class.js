4a3bd13933c6c3a7cd37cc8d8f88821c79dd56c4
Alexander Riedel <alexander.riedel@riedelmedia.de>
2024-02-17 15:02:55 +0100

    JSDOC

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -7,11 +7,19 @@ class DrawableObject {
     currentImage = 0;
     timer = 60;
 
+    /**
+     * Loads an image from the specified path and assigns it to the object's img property.
+     * @param {string} path - The path to the image file.
+     */
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    /**
+     * Loads images from an array of paths and stores them in the image cache.
+     * @param {string[]} arr - An array containing the paths to the image files.
+     */
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
@@ -20,10 +28,18 @@ class DrawableObject {
         });
     }
 
+    /**
+     * Draws the object's image onto the canvas context.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+     */
     draw(ctx) {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
+    /**
+     * Draws a blue rectangular frame around the object if it is one of the specified types.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+     */
     drawFrame(ctx) {
         if (this instanceof Character || this instanceof Chicken || this instanceof ChickenSmall || this instanceof Endboss || this instanceof Coin || this instanceof Bottle) {
             ctx.beginPath();
@@ -34,6 +50,10 @@ class DrawableObject {
         }
     }
 
+    /**
+     * Draws a red inner rectangular frame around the object if it is one of the specified types.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+     */
     drawInnerFrame(ctx) {
         if (this instanceof Character || this instanceof Chicken || this instanceof ChickenSmall || this instanceof Endboss || this instanceof Coin || this instanceof Bottle) {
             ctx.beginPath();
@@ -42,13 +62,6 @@ class DrawableObject {
             ctx.rect(this.x + this.offset.left, this.y + this.offset.top, this.width - this.offset.left - this.offset.right, this.height - this.offset.top - this.offset.bottom);
             ctx.stroke();
         }
-    }
-
-    timer() {
-        setInterval(() => {
-            this.timer--;
-            console.log(this.timer);
-        }, 1000);
     }
 
 }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -11,8 +11,6 @@ class MovableObject extends DrawableObject {
     coins = 0;
     bottles = 0;
     lastHit = 0;
-    //collect_coin_sound = new Audio('audio/coin.wav');
-    //collect_bottle_sound = new Audio('audio/bottle.wav');
     offset = {
         top: 0,
         left: 0,
@@ -20,6 +18,9 @@ class MovableObject extends DrawableObject {
         right: 0
     };
 
+    /**
+     * Applies gravity to the object by decreasing its vertical position (y-coordinate) based on its vertical speed and acceleration.
+     */
     applyGravity() {
         this.applyGravityIntervall = setInterval(() => {
             if (this.isAboveGround() || this.speedY > 0) {
@@ -29,6 +30,11 @@ class MovableObject extends DrawableObject {
         }, 1000 / 45);
     }
 
+    /**
+     * Checks if the object is above the ground. For ThrowableObject instances, it always returns true.
+     * For other objects, it returns true if the y-coordinate is less than 227.
+     * @returns {boolean} - True if the object is above the ground, false otherwise.
+     */
     isAboveGround() {
         if (this instanceof ThrowableObject) {
             return true;
@@ -37,6 +43,10 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Plays an animation by updating the image of the object based on the provided array of image paths.
+     * @param {string[]} images - Array of image paths representing the animation frames.
+     */
     playAnimation(images) {
         let i = this.currentImage++ % images.length;
         let path = images[i];
@@ -44,6 +54,10 @@ class MovableObject extends DrawableObject {
         this.currentImage++;
     }
 
+    /**
+     * Moves the object to the right by increasing its horizontal position (x-coordinate) based on its speed.
+     * @param {string} object - The type of object being moved ('character', 'enemy', etc.).
+     */
     moveRight(object) {
         if (object == 'character' && !this.isAboveGround() && this.y != 227) {
             this.y = 227;
@@ -53,6 +67,10 @@ class MovableObject extends DrawableObject {
         //level1.moveBackgroundObjects(0, this.speed);
     }
 
+    /**
+     * Moves the object to the left by decreasing its horizontal position (x-coordinate) based on its speed.
+     * @param {string} object - The type of object being moved ('character', 'enemy', etc.).
+     */
     moveLeft(object) {
         if (object == 'character' && !this.isAboveGround() && this.y != 227) {
             this.y = 227;
@@ -61,6 +79,12 @@ class MovableObject extends DrawableObject {
         //level1.moveBackgroundObjects(0, this.speed);
     }
 
+    /**
+     * Initiates a jump for the object with the given vertical and horizontal speeds.
+     * If no speeds are provided, default values are used.
+     * @param {number} [speedY=25] - The vertical speed of the jump.
+     * @param {number} [speedX=0] - The horizontal speed of the jump.
+     */
     jump(speedY, speedX) {
         if (speedY == undefined || speedX == undefined) {
             this.speedY = 25;
@@ -71,6 +95,9 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Reduces the energy of the object when hit, if its energy is greater than 0 and the last hit was more than 600 milliseconds ago.
+     */
     hit() {
         if (this.energy > 0 && new Date().getTime() - this.lastHit > 600) {
             this.energy -= 20;
@@ -78,21 +105,35 @@ class MovableObject extends DrawableObject {
         }
     }
 
+    /**
+     * Checks if the object is currently hurt based on the time since the last hit.
+     * @returns {boolean} - True if the object is hurt, false otherwise.
+     */
     isHurt() {
         let timePassed = new Date().getTime() - this.lastHit;
         timePassed = timePassed / 1000;
         return timePassed < 0.5;
     }
 
+    /**
+     * Checks if the object is dead by verifying if its energy is equal to 0.
+     * @returns {boolean} - True if the object is dead, false otherwise.
+     */
     isDead() {
         return this.energy == 0;
     }
 
+    /**
+     * Increases the number of coins collected by the object and plays a sound effect.
+     */
     collectCoin() {
         this.coins++;
         collect_coin_sound.play();
     }
 
+    /**
+     * Increases the number of bottles collected by the object and plays a sound effect.
+     */
     collectBottle() {
         this.bottles++;
         collect_bottle_sound.play();
