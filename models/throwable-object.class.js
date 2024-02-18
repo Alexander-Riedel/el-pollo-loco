75c18b40bd549d08d6f02baa7e5497418e420081
Alexander Riedel <alexander.riedel@gus-group.com>
2024-02-18 09:02:43 +0100

    JSDOC

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -16,6 +16,12 @@ class ThrowableObject extends MovableObject {
         'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
     ];
 
+    /**
+     * Creates a new bottle object that is thrown.
+     * @param {number} x - The x-coordinate where the bottle is placed.
+     * @param {number} y - The y-coordinate where the bottle is placed.
+     * @param {boolean} direction - The direction in which the bottle is thrown (true for left, false for right).
+     */
     constructor(x, y, direction) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
         this.loadImages(this.IMAGES_BOTTLE_SPLASH);
@@ -27,6 +33,10 @@ class ThrowableObject extends MovableObject {
         this.direction = direction;
     }
 
+    /**
+     * Throws the bottle in the specified direction.
+     * If the direction is true, moves the bottle to the left; otherwise, moves it to the right.
+     */
     throw() {
         this.speedY = 20;
         this.applyBottleGravity();
@@ -40,6 +50,10 @@ class ThrowableObject extends MovableObject {
         }, 1000 / 60);
     }
 
+    /**
+     * Applies gravity to the bottle while it's in the air.
+     * Updates the bottle's vertical position based on its current speed and acceleration.
+     */
     applyBottleGravity() {
         this.applyBottleGravityIntervall = setInterval(() => {
             if (this.isAboveGround() || this.speedY > 0) {
@@ -49,6 +63,11 @@ class ThrowableObject extends MovableObject {
         }, 1000 / 45);
     }
 
+    /**
+     * Checks if the character is colliding with a bottle object.
+     * @param {Object} mo - The bottle object to check collision with.
+     * @returns {boolean} - True if the character is colliding with the bottle, otherwise false.
+     */
     isCollidingWithBottle(mo) {
         return this.x + this.width - this.offset.right > mo.x + mo.offset.left &&       // R -> L
             this.y + this.height - this.offset.bottom > mo.y + mo.offset.top &&         // T -> B
@@ -56,6 +75,11 @@ class ThrowableObject extends MovableObject {
             this.y + this.offset.top < mo.y + mo.height - mo.offset.bottom              // B -> T
     }
 
+    /**
+     * Initiates the splash animation when a bottle hits an enemy.
+     * @param {Object} bottle - The bottle object.
+     * @param {Object} enemy - The enemy object.
+     */
     splashAnimation(bottle, enemy) {
         bottle.offset.top = 1000;
         clearInterval(this.throwInterval);
@@ -69,6 +93,10 @@ class ThrowableObject extends MovableObject {
         }, 1000 / 10);
     }
 
+    /**
+     * Initiates the splash animation for hitting an enemy.
+     * @param {Object} enemy - The enemy object.
+     */
     enemySplashAnimation(enemy) {
         clearInterval(this.applyBottleGravityInterval);
         this.x = enemy.x - 10;
@@ -77,6 +105,10 @@ class ThrowableObject extends MovableObject {
         this.acceleration = 0;
     }
 
+    /**
+     * Initiates the splash animation for hitting the endboss.
+     * @param {Object} bottle - The bottle object.
+     */
     endbossSplashAnimation(bottle) {
         this.x = bottle.x;
         this.y = bottle.y;
